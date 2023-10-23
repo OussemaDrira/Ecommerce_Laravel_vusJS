3626793518db093bd2ca1efd61aafafb359cad4e
OussemaDrira <oussemadrira1@gmail.com>
2023-10-23 13:39:39 +0100

    update 2 22//10/2023

---
+++ resources/js/components/accueil.vue
@@ -0,0 +1,15 @@
+<template>
+    
+    <div >
+    <h1>Vous êtes à la page d'accueil</h1>
+  
+    </div>
+    </template>
+    <script setup(props)>
+    export default {
+    mounted() {
+    console.log('Component mounted.')
+    }
+    
+    }
+    </script>
